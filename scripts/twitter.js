c4ce29197887097c39ed8ff628c2e50166abf290
Mahir Kothary <mahirk@Mahirs-MacBook-Pro.local>
2015-05-02 14:49:03 -0700

    visualization

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,14 +1,15 @@
 var margin = {top: 40, right: 40, bottom: 40, left:100},
-width = 1000 - margin.left - margin.right,
-height = 700 - margin.top - margin.bottom;
+width = $(window).width() - margin.left - margin.right,
+height = $(window).height() - margin.top - margin.bottom-200,
+interactionTypes = ["MT", "RT", "RE"];
 
 var x = d3.time.scale()
     .domain([new Date("2012-06-30"), new Date("2012-07-08")])
     .rangeRound([0, width - margin.left - margin.right]);
 
 var y = d3.scale.linear()
-    .domain([0, 1000])
-    .range([height - margin.top - margin.bottom, 0]);
+    .domain([0, 2000])
+    .range([height - margin.top - margin.bottom-20, 0]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -21,7 +22,7 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient('left')
-    .tickPadding(8);
+    .tickPadding(10);
 
 var svg = d3.select('body').append('svg')
     .attr('class', 'chart')
@@ -32,16 +33,26 @@ var svg = d3.select('body').append('svg')
 
 svg.append('g')
     .attr('class', 'x axis')
-    .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+    .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom - 20) + ')')
     .call(xAxis).append("text")
             .attr("dx", width-margin.left)
-            .attr("dy", "-1em")
+            .attr("dy", "3em")
             .style("text-anchor", "end")
-            .text("Timeline");
+            .text("Timeline - 2012");
 
 svg.append('g')
   .attr('class', 'y axis')
-  .call(yAxis);
+  .call(yAxis).append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".7em")
+  .style("text-anchor", "end")
+  .text("# Tweets ('00)");
+
+var tweetData = svg.append("text")
+    .attr("class", "tweetinfo")
+    .style({opacity: 0});
+
   var dsv = d3.dsv(" ", "text/plain");
   var data = dsv("higgs-activity_time.txt", function(error, data) {
              var vals = d3.nest()
@@ -61,64 +72,76 @@ svg.append('g')
 
             console.log(vals);
 
+            interactionTypes.forEach(function(e){
+                var tweet = svg.selectAll("."+ e + "tweet")
+               .data(vals, function(d) {
+                 return d.key;
+               });
 
-
-             var MTtweet = svg.selectAll(".tweet")
-            .data(vals, function(d) {
-              return d.key;
-            });
-
-            var RTtweet = svg.selectAll(".tweet")
-           .data(vals, function(d) {
-             return d.key;
-           });
-
-           var REtweet = svg.selectAll(".tweet")
-          .data(vals, function(d) {
-            return d.key;
-          });
-
-            MTtweet.enter().append("circle")
-           .attr("class", "MT tweet")
-           .attr("r", 6);
-
-           RTtweet.enter().append("circle")
-          .attr("class", "RT tweet")
-          .attr("r", 6);
-
-          REtweet.enter().append("circle")
-         .attr("class", "RE tweet")
-         .attr("r", 6);
-
-           MTtweet
-              .transition()
-              .duration(500)
-              .attr("cx", function(d) {
-                console.log();
-                return x(new Date(d.key));
-              })
-              .attr("cy", function(d) {
-                return y(d.values["MT"].values/100);
-              });
-              RTtweet
-                 .transition()
-                 .duration(500)
+               tweet.enter().append("circle")
+               .transition()
+               .duration(500)
+                .attr("class", e + "tweet" + " op")
+                .attr("r", 6.5)
                  .attr("cx", function(d) {
-                   console.log();
                    return x(new Date(d.key));
                  })
                  .attr("cy", function(d) {
-                   return y(d.values["RT"].values/100);
-                 });
-                 REtweet
-                    .transition()
-                    .duration(500)
-                    .attr("cx", function(d) {
-                      console.log();
-                      return x(new Date(d.key));
-                    })
-                    .attr("cy", function(d) {
-                      return y(d.values["RE"].values/100);
-                    });
+                   return y(d.values[e].values/100);
+                 }).attr("data-legend", e).delay(function(d) { return x(new Date(d.key))*1.5});
+
+            tweet.on('mouseover', function(d) {
+              tweetData
+              .attr("x", x(new Date(d.key)) + 10)
+              .attr("y", y(d.values[e].values/100) + 5)
+              .text(d.values[e].values + "  " + e + "'s")
+              .style({opacity: 1})
+            });
+
+
+            tweet.on('mouseout', function(d) {
+                tweetData.style({opacity: 0});
+              });
+
+
+                legend = svg.append("g")
+                    .attr("class","legend")
+                    .attr("transform","translate(50,30)")
+                    .style("font-size","12px")
+                    .call(d3.legend)
+
+                  setTimeout(function() {
+                    legend
+                      .style("font-size","15px")
+                      .attr("data-style-padding",15)
+                      .call(d3.legend)
+                  },1000)
+
+
+          });
 
   });
+
+  function redo(){
+    $('.REtweet').css('opacity', 1);
+    $('.RTtweet').css('opacity', 1);
+    $('.MTtweet').css('opacity', 1);
+  }
+
+  function mt(){
+    $('.REtweet').css('opacity', 0);
+    $('.RTtweet').css('opacity', 0);
+    $('.MTtweet').css('opacity', 1);
+  }
+
+  function rt(){
+    $('.REtweet').css('opacity', 0);
+    $('.RTtweet').css('opacity', 1);
+    $('.MTtweet').css('opacity', 0);
+  }
+
+  function re(){
+    $('.REtweet').css('opacity', 1);
+    $('.RTtweet').css('opacity', 0);
+    $('.MTtweet').css('opacity', 0);
+  }
